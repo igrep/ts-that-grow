d821e973b9d1a20d661ec3a473f39bed94fd2580
YAMAMOTO Yuji <yuji-yamamoto@iij.ad.jp>
2023-03-14 17:04:28 +0900

    Improve the example value to avoid copying the output from ChatGPT

--- src/app-global-interface.ts
+++ src/app-global-interface.ts
@@ -23,6 +23,7 @@ declare global {
 
 type MyExpression = Expression<"namespace">;
 
+// A trivial example transformation of an Expression to a MyExpression
 function setNamespaceFoo(expression: Expression): MyExpression {
   switch (expression.type) {
     case "Literal":
@@ -52,29 +53,46 @@ function setNamespaceFoo(expression: Expression): MyExpression {
   }
 }
 
-console.log(
-  setNamespaceFoo(
-    // 例として用意した値はChatGPTに作ってもらいました。
-    // JavaScript風の擬似コードで表すとこんな感じらしい:
-    //   x = ((y) => 42)(z);
-    {
-      type: "SetVariable",
-      name: "x",
-      value: {
-        type: "CallFunc",
-        function: {
-          type: "Func",
-          argumentName: "y",
-          body: {
-            type: "Literal",
-            value: 42,
-          },
-        },
-        argument: {
-          type: "Variable",
-          name: "z",
-        },
+// In this fictious language, a program is just a sequence of expressions.
+type Program = Expression[];
+
+type MyProgram = MyExpression[];
+
+function setNamespaceFooToProgram(statements: Program): MyProgram {
+  return statements.map((statement) => setNamespaceFoo(statement));
+}
+
+// In pseudocode:
+//   func = (arg) => 42;
+//   x = func(y);
+const statements: Program = [
+  {
+    type: "SetVariable",
+    name: "func",
+    value: {
+      type: "Func",
+      argumentName: "arg",
+      body: {
+        type: "Literal",
+        value: 42,
       },
     },
-  ),
-);
+  },
+  {
+    type: "SetVariable",
+    name: "x",
+    value: {
+      type: "CallFunc",
+      function: {
+        type: "Variable",
+        name: "func",
+      },
+      argument: {
+        type: "Variable",
+        name: "y",
+      },
+    },
+  },
+];
+
+console.log(JSON.stringify(setNamespaceFooToProgram(statements), undefined, 4));
